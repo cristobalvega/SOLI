2be87c1aa8069cb6032d080213f9e82bd28d73d8
Cristobal Vega <cristobalvega96@gmail.com>
2017-06-25 01:25:23 -0600

    Cambios menores
    Se cambio el nombre a las variables, así como el texto a los botones.

--- src/GUI/WindowFX.java
+++ src/GUI/WindowFX.java
@@ -11,58 +11,63 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+
 
 /**
  * Created by cristobalvega on 6/24/17.
  */
 
 public class WindowFX {
-        public BorderPane rootVentanaCompilador;
-        public Stage window;
-        public Scene scene;
+        public BorderPane rootPrincipal;
+        public Stage stagePrincipal;
+        public Scene scenePrincipal;
 
-        public void initGUI(Stage stage) {
+    public void initGUI(Stage stage) {
             ///////Ventana
-            window = stage;
-            rootVentanaCompilador = new BorderPane();
-            scene = new Scene(rootVentanaCompilador, 1250, 600);
-            rootVentanaCompilador.setCenter(WindowFX());
+            stagePrincipal = stage;
+            rootPrincipal = new BorderPane();
+            scenePrincipal = new Scene(rootPrincipal, 1250, 600);
+            rootPrincipal.setCenter(WindowFX());
 
             /////////////////////////////El menu
             MenuBar  menuBar = new MenuBar();
 
             //////////////////////////////////////Secciones del menu
-            Menu ArchivoMenu = new Menu("Archivo");
-            Menu EdicionMenu = new Menu("Editar");
-            Menu VerMenu = new Menu("Ver");
-            Menu AyudaMenu = new Menu("Ayuda");
+            Menu menuFile = new Menu("File");
+            Menu menuOptions = new Menu("Options");
+            Menu menuHelp= new Menu("Help");
 
             ///////////////////////////////////////Items del menu
-            MenuItem Item1Archivo = new MenuItem("K onda ");
-            MenuItem Item1Edicion = new MenuItem("K onda pa editar ");
-            MenuItem Item1Ver = new MenuItem("K onda pa ver ");
-            MenuItem Item1Ayuda = new MenuItem("K onda pa ayudar ");
+            MenuItem menuItemFile1 = new MenuItem("New");
+            MenuItem menuItemFile2 = new MenuItem("Open");
+            MenuItem menuItemOptions1  = new MenuItem("Compile");
+            MenuItem menuItemHelp1 = new MenuItem("About...");
+            menuItemHelp1.setOnAction(e->about());
 
             ///////////////////////////////////////////////////////Agregar Items al menu
-            ArchivoMenu.getItems().add(Item1Archivo);
-            EdicionMenu.getItems().add(Item1Edicion);
-            VerMenu.getItems().add(Item1Ver);
-            AyudaMenu.getItems().add(Item1Ayuda);
+            menuFile.getItems().addAll(menuItemFile1,menuItemFile2);
+            menuOptions.getItems().add(menuItemOptions1);
+            menuHelp.getItems().add(menuItemHelp1);
 
             /////////////////////////////////////////////////Agregar seccion a la barra de menu
-            menuBar.getMenus().addAll(ArchivoMenu,EdicionMenu,VerMenu,AyudaMenu);
+            menuBar.getMenus().addAll(menuFile,menuOptions,menuHelp);
             //////////////////////////////////////////////////
-            stage.setTitle("VentanaAutomata");
-            rootVentanaCompilador.setTop(menuBar);
-            stage.setScene(scene);
-            window.setResizable(false);
+            stage.setTitle("SOLI");
+            rootPrincipal.setTop(menuBar);
+            stage.setScene(scenePrincipal);
+            stagePrincipal.setResizable(false);
             stage.show();
-        }
+        }//initGUI
 
-        public VBox WindowFX() {
+    private void about() {
+    }//about
+
+    public VBox WindowFX() {
 
             VBox rootM = new VBox(10);
             HBox root2M = new HBox(20);
+
             GridPane gridpane = new GridPane();
             gridpane.setPadding(new Insets(10,15,20,15));
             gridpane.setHgap(0);
@@ -73,7 +78,6 @@ public class WindowFX {
             root2M.setPadding(new Insets(10, 0, 0, 20));
             rootM.setAlignment(Pos.CENTER);
             gridpane.setAlignment(Pos.CENTER);
-
 
             Button Archivobtn = new Button("Archivo");
             Archivobtn.setPrefHeight(80);
@@ -93,21 +97,16 @@ public class WindowFX {
             Label Salidalbl = new Label("SALIDA:");
             // rootM.getChildren().addAll(gridpane);
             gridpane.add(CodigoTxt, 0, 0);
-            CodigoTxt.setPrefWidth((int)scene.getWidth());
-            CodigoTxt.setPrefHeight((int)scene.getHeight());
+            CodigoTxt.setPrefWidth((int)scenePrincipal.getWidth());
+            CodigoTxt.setPrefHeight((int)scenePrincipal.getHeight());
             gridpane.add(Salidalbl, 0, 1);
             gridpane.add(ConsolaTxt, 0, 2);
-            ConsolaTxt.setPrefHeight((int)scene.getHeight()-300);
-            ConsolaTxt.setPrefWidth((int)scene.getWidth());
+            ConsolaTxt.setPrefHeight((int)scenePrincipal.getHeight()-300);
+            ConsolaTxt.setPrefWidth((int)scenePrincipal.getWidth());
             root2M.getChildren().addAll(Archivobtn,Edicionbtn,Verbtn,Ayudabtn);
             rootM.getChildren().addAll(root2M,gridpane);
-
-
-
             rootM.setStyle("-fx-background-color: rgba(52,50,54,0.85)");
             Salidalbl.setStyle("-fx-text-fill: white");
             return rootM;
-
-
         }//Constructor
 }//WindowFX
